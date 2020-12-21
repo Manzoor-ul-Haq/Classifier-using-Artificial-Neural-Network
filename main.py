3a6f258f79c7ff18df58f398a6389ecbc2bf465b
manzoor-ul-haq <manzoor.haq00@gmail.com>
2020-12-21 23:11:53 +0500

    updateWeights

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 x0 = 1
-W = [-0.6, 0.75, 0.5] # initial weights
-l_r = 0.02 # learning rate
+Weights = [-0.6, 0.75, 0.5] # initial weights
+learningRate = 0.02 # learning rate
 epochs = 30 #total iterations over the training data
 
 trainInput = [[1,1],[9.4,6.4],[2.5,2.1],[8,7.7],[0.5,2.2],[7.9,8.4],[7,7],[2.8,0.8],[1.2,3],[7.8,6.1]]
@@ -21,9 +21,12 @@ def activationFunction(Output):
 	else:
 		return -1
 
-def actualOutput(W, x1, x2):
-	Output = (W[0] * x0) + (W[1] * x1) + (W[2] * x2)
+def actualOutput(Weights, X):
+	Output = (Weights[0] * x0) + (Weights[1] * X[0]) + (Weights[2] * X[1])
 	return activationFunction(Output)
 
-def learning(output, x1, x2):
-	pass
+def updateWeights(actual_Output, desiredOutput, X, Weights):
+	Weights[0] = Weights[0] + (l_r * (desiredOutput - actual_Output) * x0)
+	Weights[1] = Weights[1] + (l_r * (desiredOutput - actual_Output) * X[0])
+	Weights[2] = Weights[2] + (l_r * (desiredOutput - actual_Output) * X[1])
+	return Weights
