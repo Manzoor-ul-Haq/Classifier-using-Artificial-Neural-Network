f6629bdb309ed6d1d30e2ec8aae8df5b01f32165
manzoor-ul-haq <manzoor.haq00@gmail.com>
2020-12-23 12:07:00 +0500

    Up-to-date

--- main.py
+++ main.py
@@ -1,47 +1,79 @@
-x0 = 1
-Weights = [-0.6, 0.75, 0.5] # initial weights
-learningRate = 0.02 # learning rate
-epochs = 30 #total iterations over the training data
+import matplotlib.pyplot as plt
+import numpy as np
 
-trainInput = [[1,1],[9.4,6.4],[2.5,2.1],[8,7.7],[0.5,2.2],[7.9,8.4],[7,7],[2.8,0.8],[1.2,3],[7.8,6.1]]
-trainOutput = [1, 1 ,1, -1, -1, -1, -1, 1, -1, 1]  #Desired Output
+class Perceptron():
+	def __init__(self):
+		self.x0 = 1
+		self.W = [-0.6, 0.75, 0.5] # initial weights
+		self.l_r = 0.02 # learning rate
+		self.epochs = 30 #total iterations over the training data
 
-def givenData():
-	for i in range(10):
-		for j in range(2):
-			if i == 0 and j == 0:
-				print("x1\tx2\tDesired")
-			print(trainInput[i][j], end = '\t')
-		print(trainOutput[i], end = '\t')
-		print()
+		self.trainingInput = [[1,1],[9.4,6.4],[2.5,2.1],[8,7.7],[0.5,2.2],[7.9,8.4],[7,7],[2.8,0.8],[1.2,3],[7.8,6.1]]
+		self.trainingOutput = [1, 1 ,1, -1, -1, -1, -1, 1, -1, 1]  #Desired Output
+	def givenData(self, trainInput, trainOutput):
+		for i in range(10):
+			for j in range(2):
+				if i == 0 and j == 0:
+					print("x1\tx2\tDesired")
+				print(trainInput[i][j], end = '\t')
+			print(trainOutput[i], end = '\t')
+			print()
 
-def activationFunction(Output):
-	if Output >= 0:
-		return 1
-	else:
-		return -1
+	def activationFunction(self, Output):
+		if Output >= 0:
+			return 1
+		else:
+			return -1
 
-def actualOutput(Weights, X):
-	Output = (Weights[0] * x0) + (Weights[1] * X[0]) + (Weights[2] * X[1])
-	return activationFunction(Output)
+	def actualOutput(self, Weights, X):
+		Output = (Weights[0] * self.x0) + (Weights[1] * X[0]) + (Weights[2] * X[1])
+		return self.activationFunction(Output)
 
-def updateWeights(actual_Output, desired_Output, X, Weights):
-	Weights[0] = Weights[0] + (l_r * (desiredOutput - actual_Output) * x0)
-	Weights[1] = Weights[1] + (l_r * (desiredOutput - actual_Output) * X[0])
-	Weights[2] = Weights[2] + (l_r * (desiredOutput - actual_Output) * X[1])
-	return Weights
+	def updateWeights(self, actual_Output, desired_Output, X, Weights, learningRate):
+		updatedWeights = [0, 0, 0]
+		updatedWeights[0] = Weights[0] + (learningRate * (desired_Output - actual_Output) * self.x0)
+		updatedWeights[1] = Weights[1] + (learningRate * (desired_Output - actual_Output) * X[0])
+		updatedWeights[2] = Weights[2] + (learningRate * (desired_Output - actual_Output) * X[1])
+		return updatedWeights
 
-def desiredOutput(trainOutput, X):
-	for i in range(10):
-		if trainInput[i] == X:
-			index = i
-			break
-	return trainOutput[index]
+	def desiredOutput(self, trainInput, trainOutput, X):
+		for i in range(10):
+			if trainInput[i] == X:
+				index = i
+				break
+		return trainOutput[index]
 
-def learning(Weights, trainInput, trainOutput):
-	for X in trainInput:
-		actual_Output = actualOutput(Weights, X)
-		desired_Output = desiredOutput(trainOutput, X)
-		# if desired minus actual output is double equal to zero then its means we have learnt a classifier line
-		# else we have to updateWeights....
-		#InShaAllah will do tomorrow.
+	def learning(self, Weights, trainInput, trainOutput, learningRate):
+		count = 0
+		while True:
+			previousWeights = Weights
+			count += 1
+			for X in trainInput:
+				actual_Output = self.actualOutput(Weights, X)
+				desired_Output = self.desiredOutput(trainInput, trainOutput, X)
+				Weights = self.updateWeights(actual_Output, desired_Output, X, Weights, learningRate)
+			if Weights == previousWeights:
+				return Weights, count
+
+	def graph(self, Weights, trainInput):
+		x = np.linspace(0,10,100)
+		y = -((Weights[1] * x) + (Weights[0] * 1)) / Weights[2]
+		plt.scatter(*zip(*trainInput))
+		plt.plot(x, y, '-r', label='Classifier')
+		plt.title('Classifier')
+		plt.xlabel('x', color='#1C2833')
+		plt.ylabel('y', color='#1C2833')
+		plt.legend(loc='upper left')
+		plt.grid()
+		plt.show()
+
+	def main(self):
+		self.givenData(self.trainingInput, self.trainingOutput)
+		Weights, self.epochs = self.learning(self.W, self.trainingInput, self.trainingOutput, self.l_r)
+		print("Epochs: ", self.epochs)
+		self.graph(Weights, self.trainingInput)
+
+def main():
+	Perceptron().main()
+
+main()
